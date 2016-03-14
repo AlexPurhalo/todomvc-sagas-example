17a961e6562cb351c019d1b151fc05c567b2b428
Mike Wilcox <mwilcox56@gmail.com>
2016-03-13 21:15:18 -0400

    split out complete todo

--- sagas/index.js
+++ sagas/index.js
@@ -5,35 +5,8 @@ import watchAddTodo from './add-todo'
 import watchEditTodo from './edit-todo'
 import watchDeleteTodo from './delete-todo'
 import watchFetchTodos from './fetch-todos'
+import watchCompleteTodo from './complete-todo'
 import watchClearCompletedTodos from './clear-completed'
-
-export function* completeTodo(action) {
-  const { id, completed: c } = action
-
-  // why does this get called on init?
-  if (!id) return
-
-  try {
-    const todo = yield call(
-      api, 
-      '/complete-todo', 
-      { 
-        method: 'POST', 
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id }) 
-      }
-    )
-
-    const { completed } = todo
-
-    yield put({ type: ActionTypes.COMPLETE_TODO_SUCCEEDED, id, completed })
-  } catch (e) {
-    yield put({ type: ActionTypes.COMPLETE_TODO_FAILED, id, c })
-  }
-}
 
 export function* completeAllTodos(action) {
   // todo pass all todos
@@ -75,7 +48,7 @@ export default function* watchMany() {
     fork(watchEditTodo),
     fork(watchFetchTodos),
     fork(watchDeleteTodo),
-    takeLatest(ActionTypes.COMPLETE_TODO_REQUESTED, completeTodo),
+    fork(watchCompleteTodo),
     takeLatest(ActionTypes.COMPLETE_ALL_REQUESTED, completeAllTodos),
     fork(watchClearCompletedTodos)
   ]
