52fe94bbd140ae4d76a468fbcdd76c2880b7264c
Mike Wilcox <mwilcox56@gmail.com>
2016-03-13 21:18:43 -0400

    remove api fn

--- sagas/index.js
+++ sagas/index.js
@@ -9,16 +9,6 @@ import watchCompleteTodo from './complete-todo'
 import watchCompleteAllTodos from './complete-all-todos'
 import watchClearCompletedTodos from './clear-completed'
 
-function api(url, opts) {
-  return fetch(url, opts)
-    .then(function (resp) {
-      return resp.json()
-    })
-    .then(function (resp) {
-      return resp
-    })
-}
-
 export default function* watchMany() {
   yield [
     fork(watchAddTodo),
